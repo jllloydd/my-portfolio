e51e4f93513890ef52c7b74f93302842b52959ed
jllloydd <wayfivet@gmail.com>
2024-12-08 22:10:54 +0800

    Application set-up

---
+++ src/app/components/Greeting.jsx
@@ -0,0 +1,7 @@
+export default function Greeting() {
+    return (
+      <div className="text-center">
+        <h1 className="text-2xl font-bold">Welcome to My Next.js App!</h1>
+      </div>
+    );
+  }
