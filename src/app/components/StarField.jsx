2e6c52a2313cd1593165e60b5695a03b8c71fc66
jllloydd <wayfivet@gmail.com>
2024-12-10 22:25:28 +0800

    removed unnecessary code

--- src/app/components/StarField.jsx
+++ src/app/components/StarField.jsx
@@ -4,15 +4,16 @@ import React, { useEffect, useRef } from "react";
 export default function StarField() {
   const canvasRef = useRef(null);
 
+  //Starfield initialization
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-    const stars = 200; // Number of stars
-    const starColor = "#ffffff"; // Color of the stars
+    const stars = 200;
+    const starColor = "#ffffff";
 
-    // Set canvas size
-    const canvasWidth = window.innerWidth * 1.0; // 60% of the window width
-    const canvasHeight = 500; // Adjust height as needed
+    // Canvas sizing
+    const canvasWidth = window.innerWidth * 1.0;
+    const canvasHeight = 500;
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
 
@@ -21,7 +22,7 @@ export default function StarField() {
       for (let i = 0; i < stars; i++) {
         const x = Math.random() * canvasWidth;
         const y = Math.random() * canvasHeight;
-        const radius = Math.random() * 2.5; // Random radius for stars
+        const radius = Math.random() * 2.5;
         context.beginPath();
         context.arc(x, y, radius, 0, Math.PI * 2);
         context.fillStyle = starColor;
@@ -36,24 +37,23 @@ export default function StarField() {
     <section
       id="starfield"
       style={{
-        position: "relative", // Ensure the section is positioned correctly
+        position: "relative",
         top: 0,
         right: 0,
-        height: "500px", // Adjust height as needed
-        zIndex: -5, // Ensure it is behind other elements
-        overflow: "hidden", // Hide any overflow
-        boxSizing: "border-box", // Include padding and border in the element's total width and height
+        height: "500px",
+        zIndex: -5,
+        overflow: "hidden",
+        boxSizing: "border-box",
       }}
     >
-      <canvas 
-        ref={canvasRef} 
+      <canvas
+        ref={canvasRef}
         style={{
-          display: "block", // Remove any default inline styling
-          width: "100%", // Ensure the canvas takes full width of the section
-          height: "100%", // Ensure the canvas takes full height of the section
+          display: "block",
+          width: "100%",
+          height: "100%",
         }}
       />
     </section>
-    
   );
 }
