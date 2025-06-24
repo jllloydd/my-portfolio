f788cd48176d6fc0e30315f82b8fc4f6187ac652
jllloydd <wayfivet@gmail.com>
2025-06-24 10:56:12 +0800

    Analytics test

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import ProjectsSection from "./components/ProjectsSection.jsx";
 import AuroraHero from "./components/AuroraHero.jsx";
 import StarField from "./components/StarField.jsx";
 import MobileSocials from "./components/MobileSocials.jsx";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       </div>
       <AuroraHero />
       <MobileSocials />
+      <Analytics />
     </main>
   );
 }
